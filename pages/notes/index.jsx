1ddebaae5852fcf4ef0bd2713fb22a9380b09ec7
Simone <startae14@gmail.com>
2021-03-23 00:37:04 +0100

    06. link betweend pages, even with dynamic routes

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Page = () => <div>Notes index path</div>;
+const Page = () => (
+  <div>
+    <h1>
+      Notes index path
+    </h1>
+    <Link href="/notes/[id]" as={`/notes/1`}>
+      <a>
+        Note 1
+      </a>
+    </Link>
+  </div>
+);
 
 export default Page;
