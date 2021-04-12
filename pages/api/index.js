c626db19d2f9e00c207dcef8a0931a94f3dead09
Simone <startae14@gmail.com>
2021-04-12 22:50:30 +0200

    17. basic API handler created for the endpoint /api/

---
+++ pages/api/index.js
@@ -0,0 +1,8 @@
+// pages/api/index.js
+// route => /api/
+
+export default (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ message: 'hello' }))
+}
