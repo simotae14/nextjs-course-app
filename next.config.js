69dcbc6d496399e4ad670324e0fc87d84a97184d
Simone <startae14@gmail.com>
2021-04-12 10:25:25 +0200

    14. use the next-env and dotenv-load libraries to define the ENV VARS easy in the next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-// basic config ENV vars
-module.exports = {
-  env: {
-    MY_ENV: 'KJLJLJJ'
-  }
-};
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+
+// load the env vars
+dotenvLoad();
+
+// add the env vars to the config file
+const withEnv = nextEnv();
+/**
+ * equals to
+ * {
+ *   env: {
+ *     HELP_APP_URL: https://frontendmasters.com
+ *   }
+ * }
+ */
+
+// exports the config with plugins
+module.exports = withEnv();
